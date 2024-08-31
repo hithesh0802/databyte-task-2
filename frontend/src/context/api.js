756d63923c77a55d86b8378d0682379b6510c43f
hithesh0802 <150377462+hithesh0802@users.noreply.github.com>
2024-08-31 22:25:12 +0530

    Update api.js

--- frontend/src/context/api.js
+++ frontend/src/context/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://databyte-task-2.onrender.com' ;
+const API_URL = 'https://databyte-task-2.onrender.com/api' ;
 
 export const registerUser = async (userData) => {
     const response = await axios.post(`${API_URL}/auth/register`, userData);
