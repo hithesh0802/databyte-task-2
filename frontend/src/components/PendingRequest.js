f795e6ab98a81fdc967e136cbd41fc40979c3c78
hithesh0802 <150377462+hithesh0802@users.noreply.github.com>
2024-08-31 22:04:37 +0530

    Update PendingRequest.js

--- frontend/src/components/PendingRequest.js
+++ frontend/src/components/PendingRequest.js
@@ -5,7 +5,7 @@ import '../styling/pendingreq.css';
 
 const PendingRequests = () => {
   const [pendingRequests, setPendingRequests] = useState([]);
-  const API_URL = 'https://databyte-task-2-1.onrender.com';
+  const API_URL = 'https://databyte-task-2.onrender.com';
   const [curruser,setCurruser]= useState([]);
 
   const getPendingRequests = async () => {
