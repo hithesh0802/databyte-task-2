38473105bb0d5608634cfde2c5dc9050b89e2abe
hithesh0802 <baahubali00@gmail.com>
2024-08-31 20:08:52 +0530

    upload

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const createError = require('http-errors');
 const jwt = require('jsonwebtoken');
 
